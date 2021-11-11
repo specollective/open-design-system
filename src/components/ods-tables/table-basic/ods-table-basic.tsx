2ce252b2aac8fa3773495a61585cd7b0fddbc0d3
devludaley <daleyluis@gmail.com>
2021-11-11 14:09:17 -0500

    create component ods-table-basic

---
+++ src/components/ods-tables/table-basic/ods-table-basic.tsx
@@ -0,0 +1,45 @@
+import { Component, Host, Prop, h } from '@stencil/core';
+
+@Component({
+  tag: 'ods-table-basic',
+  styleUrl: 'ods-table-basic.scss',
+  shadow: true
+})
+
+export class ODSTableBasic {
+  @Prop() class: string;
+  @Prop() headless: boolean;
+
+  render() {
+    // console.log(window.headlessmode)
+    // console.log(window)
+    const defaultClass = 'ods-table-basic-default'
+    const currentClass = this.headless
+      ? this.class : `${this.class} ${defaultClass}`
+    return (
+      <Host>
+        {/* <div> */}
+        <table class={currentClass}
+        // style={{
+        // }}
+        >
+          {/* <tr>
+            <th>Person 1</th>
+            <th>Person 2</th>
+            <th>Person 3</th>
+            <th>Person 3</th>
+            <th>Person 3</th>
+            <th>Person 3</th>
+          </tr> */}
+          <tr>
+            {/* <td></td>
+            <td>USD</td>
+            <td>EUR</td> */}
+            <slot />
+          </tr>
+        </table>
+        {/* </div> */}
+      </Host >
+    )
+  }
+}
