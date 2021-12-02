d3e5b97b783189bc0499c6ad6df237839a24fd0f
Lu Daley <46202132+DevLuDaley@users.noreply.github.com>
2021-12-02 10:51:31 -0500

    cleanup dead-code and add test for props

--- src/components/ods-tables/table/ods-table.e2e.tsx
+++ src/components/ods-tables/table/ods-table.e2e.tsx
@@ -5,39 +5,21 @@ describe('ods-table', () => {
   it('ods-table and it\'s contents have rendered correctly', async () => {
     const page = await newE2EPage();
     await page.setContent(`
-      <ods-table >
+      <ods-table>
         <ods-thead>
           <ods-row>
             <ods-header>Person 1</ods-header>
             <ods-header>Person 2</ods-header>
             <ods-header>Person 3</ods-header>
-            <ods-header>Person 4</ods-header>
-            <ods-header>Person 5</ods-header>
-            <ods-header>Person 6</ods-header>
           </ods-row>
         </ods-thead>
       </ods-table>
     `);
     const elm = await page.find('ods-table');
     expect(elm).toEqualText(`
-Person 1 Person 2 Person 3 Person 4 Person 5 Person 6
+Person 1 Person 2 Person 3
       `);
   });
-
-  // it('renders ods-table props', async () => {
-  //   const page = await newE2EPage();
-  //   await page.setContent(`
-  //     <ods-table class="healthy">
-  //     </ods-table>
-  //   `);
-  //   const compy = await page.find('ods-table');
-  //   compy.setProperty('headless', 'true');
-  //   await page.waitForChanges();
-  //   expect(compy).toHaveClass('healthy');
-
-  //   const component = await page.find('ods-table');
-  //   const element = await page.find('ods-table');
-  // });
 
   it('ods-table has been assigned the appropriate class', async () => {
     const page = await newE2EPage();
@@ -45,14 +27,6 @@ Person 1 Person 2 Person 3 Person 4 Person 5 Person 6
     await page.setContent(`
       <ods-table class="coins">
         <ods-thead class="food">
-          // <ods-row>
-            // <ods-header>Person 1</ods-header>
-            // <ods-header>Person 2</ods-header>
-            // <ods-header>Person 3</ods-header>
-            // <ods-header>Person 4</ods-header>
-            // <ods-header>Person 5</ods-header>
-            // <ods-header>Person 6</ods-header>
-          // </ods-row>
         </ods-thead>
       </ods-table>
     `);
@@ -67,67 +41,44 @@ Person 1 Person 2 Person 3 Person 4 Person 5 Person 6
     expect(coins).not.toBeNull();
   });
 
-  // it('a @PROP() is set on the ods-table', async () => {
-  //   const props = {
-  //     first: 'Marty',
-  //     lastName: 'McFly',
-  //   };
-  //   const page = await newE2EPage();
-  //   await page.setContent(`
-  //   <ods-table class="pals" status="true">
-  //      <ods-thead class="food">
-  //         // <ods-row>
-  //           // <ods-header>Person 1</ods-header>
-  //           // <ods-header>Person 2</ods-header>
-  //           // <ods-header>Person 3</ods-header>
-  //           // <ods-header>Person 4</ods-header>
-  //           // <ods-header>Person 5</ods-header>
-  //           // <ods-header>Person 6</ods-header>
-  //         // </ods-row>
-  //       </ods-thead>
-  //   </ods-table>`)
-  //   const table = page.find('ods-table');
-  //   // page.$eval('ods-table', (elm: any) => {
-  //   // within the browser's context
-  //   // let's set new property values on the component
-  //   // elm.headless = 'Marty';
-  //   // elm.lastName = 'McFly';
-  //   // });
-  //   await page.waitForChanges();
-  //   // expect(page).toHaveClass('food');
-  //   expect(table).toEqualText(``)
-
-  // });
-
-  it('ods-table renders props like a boss', async () => {
+  it('ods-table renders props', async () => {
     const page = await newE2EPage();
     const props = {
-      "<ods-row>": 'Marty',
-      // lastName: 'McFly',
+      className: "custom-class",
+      headless: "true"
     };
+
     await page.setContent(`
-      <ods-table class="coins">
+      <ods-table class="coins ${props.className}" headless=${props.headless}>
         <ods-thead class="food">
-          // <ods-row>
-            // <ods-header>Person 1</ods-header>
-            // <ods-header>Person 2</ods-header>
-            // <ods-header>Person 3</ods-header>
-            // <ods-header>Person 4</ods-header>
-            // <ods-header>Person 5</ods-header>
-            // <ods-header>Person 6</ods-header>
-          // </ods-row>
         </ods-thead>
       </ods-table>
     `);
+    const table = await page.find('ods-table');
+    const thead = await page.find('ods-thead');
+    // const coins = await page.find('.coins');
+
     await page.$eval('ods-table',
-      (elm: any, { first, lastName }) => {
-        elm.first = first;
-        elm.lastName = lastName;
+      (element: any, { className, headless }) => {
+        element.className += `coins ${className} honey`;
+        element.headless = headless;
+        console.log("element.className => ", element.className);
+        console.log("element.headless => ", element.headless);
       },
-      // props
+      props
     );
 
     await page.waitForChanges();
+    // expect(table).toHaveLength(1);
+    expect(table).toHaveClass('coins');
+    // expect(coins).not.toHaveLength(1);
+    expect(table).toHaveClass('honey');
+    expect(table).toHaveClass('custom-class');
+
+    expect(thead).not.toHaveClass('custom-class');
+    // expect(thead).toHaveClass('custom-class');  |
+    expect(thead).not.toHaveClass('coins');
+    // expect(thead).toHaveClass('food');
   });
 
 });
