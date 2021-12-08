b4d3f25c292693b5df8741765fef0bca25369491
Lu Daley <46202132+DevLuDaley@users.noreply.github.com>
2021-12-08 11:53:03 -0500

    create test `assigned the appropriate class`

--- src/components/ods-tables/table/data/ods-data.e2e.tsx
+++ src/components/ods-tables/table/data/ods-data.e2e.tsx
@@ -1,7 +1,7 @@
 import { h } from '@stencil/core';
 import { newE2EPage } from '@stencil/core/testing';
 
-describe('ods-row', () => {
+describe('ods-data', () => {
   it('rendered ods-data correctly', async () => {
     const page = await newE2EPage();
 
@@ -18,5 +18,31 @@ describe('ods-row', () => {
     const data = await page.find('ods-data');
     expect(data).not.toBeNull();
     expect(data).toEqualText(`Person 1`);
+  });
+
+  it('assigned the appropriate class', async () => {
+
+    const page = await newE2EPage();
+    const dataClass = 'data-class'
+
+    await page.setContent(`
+      <ods-table>
+        <ods-thead>
+          <ods-data class="${dataClass}">
+            <ods-header>Person 1</ods-header>
+            <ods-header>Person 2</ods-header>
+            <ods-header>Person 3</ods-header>
+          </ods-data>
+        </ods-thead>
+      </ods-table>
+    `);
+
+    const data = await page.find('ods-data');
+    const dataBaseClass = await page.find('.data-class');
+    await page.waitForChanges();
+
+    expect(data).not.toBeNull();
+    expect(data).toHaveClass('data-class');
+    expect(dataBaseClass).not.toBeNull();
   });
 });
