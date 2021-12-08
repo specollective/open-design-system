0c90287d4bb98e3d205d12d7e9132491e051b5ea
Lu Daley <46202132+DevLuDaley@users.noreply.github.com>
2021-12-08 14:42:25 -0500

    create test `assigned the appropriate class`

--- src/components/ods-tables/table/body/ods-body.e2e.tsx
+++ src/components/ods-tables/table/body/ods-body.e2e.tsx
@@ -18,4 +18,27 @@ describe('ods-body', () => {
     expect(body).toEqualText(`Person 1`);
   });
 
+  it('assigned the appropriate class', async () => {
+
+    const page = await newE2EPage();
+    const bodyClass = 'body-class'
+
+    await page.setContent(`
+      <ods-table>
+          <ods-body class="${bodyClass}">
+            <ods-row>Person 1</ods-row>
+            <ods-row>Person 2</ods-row>
+            <ods-row>Person 3</ods-row>
+          </ods-body>
+      </ods-table>
+    `);
+
+    const body = await page.find('ods-body');
+    const bodyBaseClass = await page.find('.body-class');
+    await page.waitForChanges();
+
+    expect(body).not.toBeNull();
+    expect(body).toHaveClass('body-class');
+    expect(bodyBaseClass).not.toBeNull();
+  });
 });
