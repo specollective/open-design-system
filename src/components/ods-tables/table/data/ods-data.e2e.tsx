acff4bcf3ce54a8038dd48a42fb8be38279f153a
Lu Daley <46202132+DevLuDaley@users.noreply.github.com>
2021-12-08 13:32:37 -0500

    create test `nested ods-button element...`

--- src/components/ods-tables/table/data/ods-data.e2e.tsx
+++ src/components/ods-tables/table/data/ods-data.e2e.tsx
@@ -130,5 +130,58 @@ describe('ods-data', () => {
     expect(base).not.toBeNull();
   });
 
+  it('nested ods-button element maintained it\'s class and onClick() functionality', async () => {
+    const page = await newE2EPage();
+    const props = {
+      className: "custom-class",
+      headless: false
+    };
+    const baseClass = "base-class"
+    const clickStatus = "clickable"
+    let btnMessage = "Off"
+
+    const handleClick = () => {
+      clickStatus == "clickable" ?
+        btnMessage = "On"
+        :
+        btnMessage = "Off"
+    }
+
+    await page.setContent(`
+      <ods-table>
+          <ods-body>
+                <ods-data class="${baseClass}">
+                  <ods-button class="${clickStatus}" onClick="${handleClick()}">
+                      ${btnMessage}
+                  </ods-button>
+                </ods-data>
+          </ods-body>
+      </ods-table>
+    `);
+
+    const hydrated = "hydrated"
+    const data = await page.find('ods-data');
+    const base = await page.find('.base-class');
+    const btn = await page.find('ods-button');
+
+    await page.$eval('ods-data',
+      (element: any, { className, headless }) => {
+        const baseClass = "base-class"
+        element.headless = headless;
+        element.headless ?
+          element.className = baseClass :
+          element.className += `${className} third-class`
+      },
+      props
+    );
+
+    await page.waitForChanges();
+    expect(data).not.toBeNull();
+    expect(data).toHaveClass('base-class');
+    expect(data).toHaveClasses([`${hydrated}custom-class`, "third-class"]);
+    expect(base).not.toBeNull();
+    expect(btn).toHaveClass(clickStatus);
+    expect(btnMessage).toEqualText("On");
+  });
 
 });
