f601f63b7df3957b712681cffbf885532f5f52ca
Lu Daley <46202132+DevLuDaley@users.noreply.github.com>
2021-12-09 09:44:05 -0500

    create `assigned the appropriate class'

--- src/components/ods-tables/table/foot/ods-foot.e2e.tsx
+++ src/components/ods-tables/table/foot/ods-foot.e2e.tsx
@@ -18,5 +18,27 @@ describe('ods-foot', () => {
     expect(foot).toEqualText(`Person 1`);
   });
 
+});
 
+it('assigned the appropriate class', async () => {
+
+  const page = await newE2EPage();
+  const footClass = 'foot-class'
+
+  await page.setContent(`
+      <ods-table>
+          <ods-foot class="${footClass}">
+            Person 1
+          </ods-foot>
+      </ods-table>
+    `);
+
+  const foot = await page.find('ods-foot');
+  const footBaseClass = await page.find('.foot-class');
+  await page.waitForChanges();
+
+  expect(foot).not.toBeNull();
+  expect(foot).toHaveClass('foot-class');
+  expect(footBaseClass).not.toBeNull();
+});
 });
