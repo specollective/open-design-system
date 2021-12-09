4ef3083de5dec3e5fa790b3c4e13e10fe80e6705
Lu Daley <46202132+DevLuDaley@users.noreply.github.com>
2021-12-09 11:01:52 -0500

    create `assigned the appropriate class`

--- src/components/ods-tables/table/header/ods-header.e2e.tsx
+++ src/components/ods-tables/table/header/ods-header.e2e.tsx
@@ -17,7 +17,29 @@ describe('ods-header', () => {
     expect(header).not.toBeNull();
     expect(header).toEqualText(`Person 1`);
   });
+  it('assigned the appropriate class', async () => {
 
+    const page = await newE2EPage();
+    const headerClass = 'header-class'
+
+    await page.setContent(`
+      <ods-table>
+          <ods-header class="${headerClass}">
+            <ods-row>Person 1</ods-row>
+            <ods-row>Person 2</ods-row>
+            <ods-row>Person 3</ods-row>
+          </ods-header>
+      </ods-table>
+    `);
+
+    const header = await page.find('ods-header');
+    const headerBaseClass = await page.find('.header-class');
+    await page.waitForChanges();
+
+    expect(header).not.toBeNull();
+    expect(header).toHaveClass('header-class');
+    expect(headerBaseClass).not.toBeNull();
+  });
 
 
 });
