5fbcaa19c0c89d69e1247167348902b21ee062b0
Lu Daley <46202132+DevLuDaley@users.noreply.github.com>
2021-12-10 10:23:16 -0500

    Create ods-input-text.tsx

---
+++ src/components/ods-input-text/ods-input-text.tsx
@@ -0,0 +1,32 @@
+import { Component, Host, Prop, State, h } from '@stencil/core';
+
+@Component({
+  tag: 'ods-input-text',
+  styleUrl: 'ods-input-text.scss',
+  shadow: true
+})
+export class ODSInputText {
+  @Prop() class: string;
+  @Prop() headless: boolean;
+  @State() value: string;
+
+  handleSubmit(e) {
+    e.preventDefault()
+    console.log(this.value);
+    // send data to our backend
+  }
+
+  render() {
+    const defaultClass = 'ods-input-text-default'
+    const currentClass = this.headless
+      ? this.class : `${this.class} ${defaultClass}`
+    return (
+      <Host>
+        <input type="text" class={currentClass} >
+          <slot />
+        </input>
+      </Host >
+    )
+  }
+
+}
