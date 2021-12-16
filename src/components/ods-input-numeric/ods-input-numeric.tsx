b7a256acb58733022e81abff2476045bc6526714
Lu Daley <46202132+DevLuDaley@users.noreply.github.com>
2021-12-16 14:03:56 -0500

    add handleChange() to ODS-input-numeric && input-text

--- src/components/ods-input-numeric/ods-input-numeric.tsx
+++ src/components/ods-input-numeric/ods-input-numeric.tsx
@@ -10,13 +10,24 @@ export class ODSInputNumeric {
   @Prop() headless: boolean;
   @State() value: string;
 
+  handleChange(event: any) {
+    this.value = event.target.value;
+    if (event.target.validity.typeMismatch) {
+      console.log('this element is not valid')
+    }
+    console.log(4, this.value);
+  }
+
   render() {
     const defaultClass = 'ods-input-numeric-default'
     const currentClass = this.headless
       ? this.class : `${this.class} ${defaultClass}`
     return (
       <Host>
-        <input type="number" class={currentClass} >
+        <input
+          type="number"
+          class={currentClass}
+          onInput={(event) => this.handleChange(event)}>
           <slot />
         </input>
       </Host >
--- src/components/ods-input-text/ods-input-text.tsx
+++ src/components/ods-input-text/ods-input-text.tsx
@@ -10,13 +10,21 @@ export class ODSInputText {
   @Prop() headless: boolean;
   @State() value: string;
 
+  handleChange(event: any) {
+    this.value = event.target.value;
+    if (event.target.validity.typeMismatch) {
+      console.log('this element is not valid')
+    }
+    console.log(4, this.value);
+  }
+
   render() {
     const defaultClass = 'ods-input-text-default'
     const currentClass = this.headless
       ? this.class : `${this.class} ${defaultClass}`
     return (
       <Host>
-        <input type="text" class={currentClass} >
+        <input type="text" class={currentClass} autoComplete="on" onInput={(event) => this.handleChange(event)}>
           <slot />
         </input>
       </Host >
